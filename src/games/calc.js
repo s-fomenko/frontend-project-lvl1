9c6dc5645088ed64a20776cdc52fb20396712899
Sergey Fomenko <fomenko.personal@gmail.com>
2019-11-10 20:41:26 +0300

    fix(calc): fix operator error

--- src/games/calc.js
+++ src/games/calc.js
@@ -4,20 +4,23 @@ export default (name) => {
   for (let i = 0; i < 3; i += 1) {
     const num1 = getRandomInt(1, 100);
     const num2 = getRandomInt(1, 100);
-    const type = getRandomInt(1, 3);
+    const type = getRandomInt(1, 4);
     let result = 0;
     let answer = '';
 
     if (type === 1) {
-      answer = question(`${num1} + ${num2} `);
+      answer = question(`Question: ${num1} + ${num2} `);
       result = num1 + num2;
     } else if (type === 2) {
-      answer = question(`${num1} - ${num2} `);
+      answer = question(`Question: ${num1} - ${num2} `);
       result = num1 - num2;
     } else {
-      answer = question(`${num1} * ${num2} `);
+      answer = question(`Question: ${num1} * ${num2} `);
       result = num1 * num2;
     }
+
+    console.log(`Your answer: ${answer}`);
+
     if (Number(answer) === result) {
       console.log('Correct!');
     } else {
