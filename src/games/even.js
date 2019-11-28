dd0979be995aafaa84b44dba3dd4d99d6c63a58a
Sergey Fomenko <s.fomenko@tinkoff.ru>
2019-11-28 15:05:00 +0300

    refactor(even): component refactoring

--- src/games/even.js
+++ src/games/even.js
@@ -1,24 +1,9 @@
-import {
-  getRandomInt,
-  question,
-  cons,
-  isEven,
-} from '../common';
+import {cons, getRandomInt,} from '../common';
 
-const brainEven = (name) => {
-  for (let i = 0; i < 3; i += 1) {
-    const num = getRandomInt(1, 99);
-    const answer = question(`Question: ${num} `);
-
-    console.log(`Your answer: ${answer}`);
-
-    if ((isEven(num) && answer === 'yes') || (!isEven(num) && answer === 'no')) {
-      console.log('Correct!');
-    } else {
-      return console.log(`'${answer}' is wrong answer ;(. Correct answer was '${answer === 'yes' ? 'no' : 'yes'}'. Let's try again, ${name}`);
-    }
-  }
-  return console.log(`Congratulations, ${name}!`);
+const brainEven = () => {
+  return {
+    num: getRandomInt(1, 99),
+  };
 };
 
 export default cons(brainEven, 'brainEven');
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import terms from './config';
-import { question } from './common';
+import { isEven, question } from './common';
 
 export default (gameType, termsText) => {
   console.log('Welcome to the Brain Games!');
@@ -8,5 +8,18 @@ export default (gameType, termsText) => {
   const name = question('May I have your name? ');
   console.log(`Hello, ${name}!\n`);
 
-  gameType(name);
+  for (let i = 0; i < 3; i += 1) {
+    const data = gameType();
+
+    const answer = question(`Question: ${data.num} `);
+
+    console.log(`Your answer: ${answer}`);
+
+    if ((isEven(data.num) && answer === 'yes') || (!isEven(data.num) && answer === 'no')) {
+      console.log('Correct!');
+    } else {
+      return console.log(`'${answer}' is wrong answer ;(. Correct answer was '${answer === 'yes' ? 'no' : 'yes'}'. Let's try again, ${name}`);
+    }
+  }
+  return console.log(`Congratulations, ${name}!`);
 };
