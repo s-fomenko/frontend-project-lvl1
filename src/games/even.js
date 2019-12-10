2feda0cef55dfea8170577520d44e604bcdc3754
Sergey Fomenko <fomenko.personal@gmail.com>
2019-12-10 08:13:42 +0300

    refactor(even.js): change structure

--- src/games/even.js
+++ src/games/even.js
@@ -1,10 +1,15 @@
 import { getRandomInt, cons, isEven } from '../common';
 
-const brainEven = (name) => ({
-  num: getRandomInt(1, 99),
-  answer: `Question: ${this.num} `,
-  result: (isEven(this.num) && this.answer === 'yes') || (!isEven(this.num) && this.answer === 'no') ? 'Correct!' : `'${this.answer}' is wrong answer ;(. Correct answer was '${this.answer === 'yes' ? 'no' : 'yes'}'. Let's try again, ${name}`,
-});
+const brainEven = (name) => {
+  const data = {};
+  data.num = getRandomInt(1, 99);
+  data.answer = `Question: ${data.num} `;
+  // data.check = (isEven(this.num) && this.answer === 'yes') || (!isEven(this.num) && this.answer === 'no');
+  data.correct = 'Correct!';
+  data.wrong = `'${data.answer}' is wrong answer ;(. Correct answer was '${data.answer === 'yes' ? 'no' : 'yes'}'. Let's try again, ${name}`;
+
+  return data;
+};
 
 
 export default cons(brainEven, 'brainEven');
--- src/index.js
+++ src/index.js
@@ -12,6 +12,7 @@ export default (gameType, termsText) => {
 
   for (let i = 0; i < 3; i += 1) {
     const data = gameType();
+    console.log(data);
 
     const answer = question(data.answer);
 
