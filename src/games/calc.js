2b2e80d2343a8bddf410eb2e2ff973ebb22bc1f1
Sergey Fomenko <s.fomenko@tinkoff.ru>
2019-12-11 17:46:14 +0300

    refactor(calc): refactor module

--- src/games/calc.js
+++ src/games/calc.js
@@ -5,8 +5,6 @@ const brainCalc = () => {
   data.num1 = getRandomInt(1, 100);
   data.num2 = getRandomInt(1, 100);
   data.type = getRandomInt(1, 4);
-  // let result = 0;
-  // let answer = '';
 
   switch (data.type) {
     case 1:
@@ -21,6 +19,8 @@ const brainCalc = () => {
       data.answer = `Question: ${data.num1} * ${data.num2} `;
       data.result = data.num1 * data.num2;
   }
+
+  return data;
 };
 
 export default cons(brainCalc, 'brainCalc');
