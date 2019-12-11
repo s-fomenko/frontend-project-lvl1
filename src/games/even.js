406cf067b820ebcdaa63dde3e4168ba9eca71a19
Sergey Fomenko <fomenko.personal@gmail.com>
2019-12-11 07:10:01 +0300

    refactor(prime): refactor module

--- src/games/even.js
+++ src/games/even.js
@@ -1,15 +1,11 @@
-import { getRandomInt, cons, isEven } from '../common';
+import { getRandomInt, cons } from '../common';
 
-const brainEven = (name) => {
+const brainEven = () => {
   const data = {};
   data.num = getRandomInt(1, 99);
   data.answer = `Question: ${data.num} `;
-  // data.check = (isEven(this.num) && this.answer === 'yes') || (!isEven(this.num) && this.answer === 'no');
-  data.correct = 'Correct!';
-  data.wrong = `'${data.answer}' is wrong answer ;(. Correct answer was '${data.answer === 'yes' ? 'no' : 'yes'}'. Let's try again, ${name}`;
 
   return data;
 };
-
 
 export default cons(brainEven, 'brainEven');
--- src/games/prime.js
+++ src/games/prime.js
@@ -1,5 +1,11 @@
 import { getRandomInt, cons } from '../common';
 
-const brainPrime = () => ({ num: getRandomInt(1, 99) });
+const brainPrime = () => {
+  const data = {};
+  data.num = getRandomInt(1, 99);
+  data.answer = `Question: ${data.num} `;
+
+  return data;
+};
 
 export default cons(brainPrime, 'brainPrime');
