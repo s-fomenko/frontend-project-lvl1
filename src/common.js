050c4130ac8bb93e0231ee64210ae1385f47c0ea
Sergey Fomenko <s.fomenko@tinkoff.ru>
2019-12-16 17:45:22 +0300

    refactor(prime): refactor module

--- src/common.js
+++ src/common.js
@@ -8,15 +8,15 @@ export const cons = (type, terms) => (func) => func(type, terms);
 
 // export const isEven = (num) => num % 2 === 0;
 
-export const isPrime = (num) => {
-  if (num < 2) {
-    return false;
-  }
-
-  for (let j = num - 1; j >= 2; j -= 1) {
-    if (num % j === 0) {
-      return false;
-    }
-  }
-  return true;
-};
+// export const isPrime = (num) => {
+//   if (num < 2) {
+//     return false;
+//   }
+//
+//   for (let j = num - 1; j >= 2; j -= 1) {
+//     if (num % j === 0) {
+//       return false;
+//     }
+//   }
+//   return true;
+// };
--- src/games/prime.js
+++ src/games/prime.js
@@ -1,12 +1,29 @@
 import { getRandomInt } from '../common';
 import game from '..';
 
+const termsPrime = 'Answer "yes" if given number is prime. Otherwise answer "no".\n';
+const isPrime = (num) => {
+  if (num < 2) {
+    return false;
+  }
+
+  for (let j = num - 1; j >= 2; j -= 1) {
+    if (num % j === 0) {
+      return false;
+    }
+  }
+  return true;
+};
+
+
 const brainPrime = () => {
   const data = {};
+  data.type = 'prime';
   data.num = getRandomInt(1, 99);
   data.answer = `Question: ${data.num} `;
+  data.check = isPrime(data.num);
 
   return data;
 };
 
-export default () => game(brainPrime, 'brainPrime');
+export default () => game(brainPrime, termsPrime);
