0543a4f73e0fcdde31825bf5dbf638eed072b6cf
Sergey Fomenko <s.fomenko@tinkoff.ru>
2019-12-20 11:25:01 +0300

    feat(common): change getRandomInt logics

--- src/common.js
+++ src/common.js
@@ -1,5 +1,5 @@
 export default (a, b) => {
   const min = Math.ceil(a);
   const max = Math.floor(b);
-  return Math.floor(Math.random() * (max - min)) + min;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 };
