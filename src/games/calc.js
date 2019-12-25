cbb1ec228c35c66de1d8d2106d890e796a7ec5bf
Sergey Fomenko <s.fomenko@tinkoff.ru>
2019-12-25 17:13:29 +0300

    refactor(calc): create getOperation function

--- src/games/calc.js
+++ src/games/calc.js
@@ -4,24 +4,45 @@ import game from '..';
 const termsCalc = 'What is the result of the expression?';
 const numberOfOperations = 3;
 
+const getOperation = () => {
+  const type = getRandomInt(1, numberOfOperations);
+  let operation = '';
+
+  switch (type) {
+    case 1:
+      operation = 'addition';
+      break;
+    case 2:
+      operation = 'subtraction';
+      break;
+    case 3:
+      operation = 'multiplication';
+      break;
+    // no default
+  }
+
+  return operation;
+};
+
 const brainCalc = () => {
   const data = {};
   data.num1 = getRandomInt(1, 99);
   data.num2 = getRandomInt(1, 99);
-  data.type = getRandomInt(1, numberOfOperations);
 
-  switch (data.type) {
-    case 1:
+  switch (getOperation()) {
+    case 'addition':
       data.question = `${data.num1} + ${data.num2}`;
       data.result = data.num1 + data.num2;
       break;
-    case 2:
+    case 'subtraction':
       data.question = `${data.num1} - ${data.num2}`;
       data.result = data.num1 - data.num2;
       break;
-    default:
+    case 'multiplication':
       data.question = `${data.num1} * ${data.num2}`;
       data.result = data.num1 * data.num2;
+      break;
+    // no default
   }
 
   return data;
