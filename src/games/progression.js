b9a98be45644f8466041d7be4253c4b8b50cfab3
Sergey Fomenko <s.fomenko@tinkoff.ru>
2020-01-13 17:58:39 +0300

    refactor(progression): change module structure

--- src/games/progression.js
+++ src/games/progression.js
@@ -3,6 +3,8 @@ import game from '..';
 
 const termsProgression = 'What number is missing in the progression?';
 const progressionLength = 10;
+const randomIndex = getRandomInt(0, progressionLength - 1);
+
 const createProgression = (start, step) => {
   let progressionStep = start;
   let progression = '';
@@ -14,22 +16,21 @@ const createProgression = (start, step) => {
   return progression.substr(0, progression.length - 1);
 };
 
+const getAnswer = (string) => {
+  const array = string.split(' ');
+  return Number(array[randomIndex]);
+};
+
+const hideNumber = (string) => {
+  const array = string.split(' ');
+  array[randomIndex] = '..';
+  return array.join(' ');
+};
+
 const createProgressionGame = () => {
   const start = getRandomInt(1, 99);
   const step = getRandomInt(1, 9);
-  const randomIndex = getRandomInt(0, progressionLength - 1);
   const data = {};
-
-  const getAnswer = (str) => {
-    const array = str.split(' ');
-    return Number(array[randomIndex]);
-  };
-
-  const hideNumber = (str) => {
-    const array = str.split(' ');
-    array[randomIndex] = '..';
-    return array.join(' ');
-  };
 
   data.question = hideNumber(createProgression(start, step));
   data.result = String(getAnswer(createProgression(start, step)));
