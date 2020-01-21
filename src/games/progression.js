d9c42caaadd91c8d8b785ff87de86b98d7a357c9
Sergey Fomenko <fomenko.personal@gmail.com>
2020-01-21 20:37:41 +0300

    refactor(progression): change variable name

--- src/games/progression.js
+++ src/games/progression.js
@@ -6,12 +6,12 @@ const progressionLength = 10;
 const randomIndex = getRandomInt(0, progressionLength - 1);
 
 const createProgression = (start, step) => {
-  let progressionItem = start;
+  let currentItem = start;
   let progression = '';
 
   for (let j = 0; j < progressionLength; j += 1) {
-    progressionItem += step;
-    progression = `${progression + progressionItem} `;
+    currentItem += step;
+    progression = `${progression + currentItem} `;
   }
 
   return progression.substr(0, progression.length - 1);
